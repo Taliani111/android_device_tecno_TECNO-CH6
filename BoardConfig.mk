eab9d5aa57d6570d71ada43d421a9d12b6ddcbb1
Taliani111 <131676147+Taliani111@users.noreply.github.com>
2023-06-24 22:45:25 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-CH6
+DEVICE_PATH := device/tecno/CH6
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -112,3 +112,4 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
+TW_BUILD_VERSION := BUILD NINO
